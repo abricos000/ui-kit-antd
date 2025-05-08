1e0bf36d1c2006e1d824bf2d2c16c67efa9c1734
Vasiliy <manuilov@sputnikfund.ru>
2025-05-09 04:39:18 +0700

    fix: change withTheme

--- src/lib/hocs/withTheme.tsx
+++ src/lib/hocs/withTheme.tsx
@@ -32,43 +32,6 @@
 
 
 
-// import React from 'react';
-// import { ConfigProvider } from 'antd';
-// import hoistNonReactStatics from 'hoist-non-react-statics';
-// import { defaultTheme } from '../theme/config';
-
-// export const withTheme = <T extends React.ComponentType<any>>(
-//   Component: T
-// ): React.FC<React.ComponentProps<T>> & Pick<T, keyof T> => {
-//   const WrappedComponent: React.FC<React.ComponentProps<T>> = (props) => (
-//     <ConfigProvider theme={defaultTheme}>
-//       <Component {...props} />
-//     </ConfigProvider>
-//   );
-
-//   const ResultComponent = hoistNonReactStatics(WrappedComponent, Component);
-
-//   Object.keys(Component).forEach((key) => {
-//     const nestedComponent = (Component as any)[key];
-//     if (
-//       nestedComponent &&
-//       (typeof nestedComponent === 'function' || React.isValidElement(nestedComponent))
-//     ) {
-//       (ResultComponent as any)[key] = withTheme(nestedComponent);
-//     } else {
-//       (ResultComponent as any)[key] = nestedComponent;
-//     }
-//   });
-
-//   return ResultComponent as React.FC<React.ComponentProps<T>> & Pick<T, keyof T>;
-// };
-
-
-
-
-
-
-
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import hoistNonReactStatics from 'hoist-non-react-statics';
@@ -85,19 +48,15 @@ export const withTheme = <T extends React.ComponentType<any>>(
 
   const ResultComponent = hoistNonReactStatics(WrappedComponent, Component);
 
-  // Копируем только статические свойства, которые являются React-компонентами
   Object.keys(Component).forEach((key) => {
-    const nested = (Component as any)[key];
-    
-    // Пропускаем методы API (например, notification.useNotification)
-    if (typeof nested === 'function' && !React.isValidElement(nested)) {
-      (ResultComponent as any)[key] = nested;
-    } 
-    // Оборачиваем только React-компоненты
-    else if (React.isValidElement(nested) || (nested?.$$typeof === Symbol.for('react.element'))) {
-      (ResultComponent as any)[key] = withTheme(nested);
+    const nestedComponent = (Component as any)[key];
+    if (
+      nestedComponent &&
+      (typeof nestedComponent === 'function' || React.isValidElement(nestedComponent))
+    ) {
+      (ResultComponent as any)[key] = withTheme(nestedComponent);
     } else {
-      (ResultComponent as any)[key] = nested;
+      (ResultComponent as any)[key] = nestedComponent;
     }
   });
 

6b619f6ecb3e5c9850fdc55f246a00846e8e7ebb
Manuylov <manuilov@sputnikfund.ru>
2025-05-12 17:25:57 +0700

    refact: change stories Switch

--- src/lib/theme/config.ts
+++ src/lib/theme/config.ts
@@ -2,24 +2,9 @@ import type { ThemeConfig } from 'antd';
 
 export const defaultTheme: ThemeConfig = {
   "token": {
-    "colorPrimary": "#91f246",
-    "colorInfo": "#62f9ef",
-    "colorSuccess": "#da3de6",
-    "colorWarning": "#b3801c",
-    "colorError": "#ff0004",
-    "colorLink": "#7e6f12",
-    "colorTextBase": "#040941",
-    "colorBgBase": "#f7eaea",
-    "fontSize": 16,
-    "sizeStep": 6,
-    "sizeUnit": 3,
-    "borderRadius": 8,
-    "wireframe": false,
-    "boxShadow": "0 6px 10px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12),0 9px 28px 8px rgba(0, 0, 0, 0.05)"
-  },
-  "components": {
-    "Button": {
-      "algorithm": true
-    }
+    "colorPrimary": "#ef16ff",
+    "colorInfo": "#ef16ff",
+    "colorTextBase": "#4ae954",
+    "fontSize": 21
   }
 }
